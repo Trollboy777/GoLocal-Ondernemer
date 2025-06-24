4bf688f3efed2f22cc86d2f75bd7b8fd885b82b8
Patesaus77 <Emregulec70@gmail.com>
2025-06-24 13:36:31 +0200

    live deployment

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/GoLocal-Ondernemer/',  // let op slash aan begin en einde
   plugins: [react()],
 })
